5420f3fdae4ecab4a0b9d03e043dfaa41fa86ff7
Valentin <wdcvalentin@yahoo.fr>
2020-03-22 18:51:41 +0100

    sidebar done

--- letstrip/src/components/Sidebar.js
+++ letstrip/src/components/Sidebar.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import auth from '../auth';
-import Navbar from 'react-bootstrap/Navbar'
 import Button from 'react-bootstrap/Button'
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
+import map from '../design/icons/map.svg'
+import contact from '../design/icons/contact.svg'
+import setting from '../design/icons/setting.svg'
 import '../styles/Sidebar_styles.css'
 
-class NavBar extends React.Component {
+class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+        this.wrapperRef = React.createRef()
+    }
+
+    handleClick() {
+        const wrapper = this.wrapperRef.current;
+        wrapper.classList.toggle('is-nav-open');
+    }
+
     render() {
         return (
-            <div className="sidebar">
-                <h1>Let's Trip</h1>
+            <div ref={this.wrapperRef} className="wrapper" >
+                <div className="nav">
+                    <Button className="nav__icon" type="menu-fold" onClick={() => this.handleClick()}>X</Button>
+                    <div className="nav__body">
+                        <h1>Let's Trip</h1>
+                        <ul>
+                            <li style={{ listStyleImage:`url(${map})` }}> <a href='/dashboard'> Mes RoadMaps </a></li>
+                            <li style={{listStyleImage:`url(${setting})`}}> <a href='/dashboard'>Paramétrer mon compte</a></li>
+                            <li style={{listStyleImage:`url(${contact})`}}><a href='/dashboard'>Contact</a></li>
+                            <li><a href='/' onClick={auth.logout}>Disconnect</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
-export default NavBar;
+export default Sidebar;
